721b2bf6cb440be73f2383096bc817ebb8949153
kalaiguna <kalaiguna@hotmail.com>
2021-08-17 23:02:54 +0530

    first commit

---
+++ src/myapi/Controllers/MyAPIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace myapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyAPIController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MyAPIController> _logger;
+
+        public MyAPIController(ILogger<MyAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("SayHello")]
+        public string Hello([FromQuery] string name) => "Hello " + name + "!";
+    }
+}
